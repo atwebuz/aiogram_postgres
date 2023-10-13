a78f4a00e6f3180e04f03b0cea8ba8b33d92ebd8
Atwebuz <abdusattorov2004@mail.ru>
2023-10-13 16:23:09 +0500

    upgrading

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -22,8 +22,8 @@ async def get_all_users(message: Message):
         user_full = user[1]
         user_usname = user[2]
         tg_id = user[3]
-        msg = "\n".join([f"{user[1]}"])
-        await bot.send_message(chat_id=ADMINS[0], text=f"{user_id,user_full, user_usname, tg_id,msg}")
+        msg = "\n".join([f"{user[1],user_id,user_full, user_usname, tg_id}"])
+        await bot.send_message(chat_id=ADMINS[0], text=f"{msg}")
         await asyncio.sleep(0.05)
     await bot.send_message(chat_id=ADMINS[0], text=f"foydalanuvchi soni: {count} ta")
     
